5499f5b4146801372952cf63ad4e76d7b9ca99f6
pedrodimarco <68877732+pedrodimarco@users.noreply.github.com>
2021-05-26 19:30:21 -0300

    Inicio bucle del juego

--- obligatorio.c
+++ obligatorio.c
@@ -6,8 +6,10 @@ int main() {
 
     srand(time(NULL));
 
-    int pos1, pos2, cantArboles = 0;
+    int pos1, pos2, cantArboles = 0, vidaA = 1, vidaB = 1;
+    int coin = rand()%2; //Variable que simula el giro de una moneda con 2 resultados aleatorios posibles
     char bosque[10][10];
+    char jugador;
 
     //Se colocan 10 arboles al azar
     //(Los arboles estan representados por los '0')
@@ -28,7 +30,7 @@ int main() {
         }
     }
     
-    //Inicializo a los jugadores A y B en sus posiciones
+    //Inicializo a los jugadores A y B en sus posiciones iniciales
     for(pos2 = 0; pos2 <= 9; pos2++) {
 		
 		for(pos1 = 0; pos1 <= 9; pos1++){
@@ -45,14 +47,32 @@ int main() {
 		}
 	}
 
-    //Imprimo en pantalla el bosque/tablero
-    for(pos2 = 0; pos2 <= 9; pos2++) {
+    //Bucle del juego
+    do { 
+        //Limpio pantalla e imprimo el tablero de juego
+        //(Si es usuario de linux usar cambiar "cls" por "clear")
+        system("cls");
+        printf("\n");
+        for(pos2 = 0; pos2 <= 9; pos2++) {
 		
-		for(pos1 = 0; pos1 <= 9; pos1++){ 
-            printf("|%c ", bosque[pos1][pos2]);
+		    for(pos1 = 0; pos1 <= 9; pos1++){ 
+                printf("|%c ", bosque[pos1][pos2]);
+            }
+            printf("|\n");
         }
-        printf("|\n");
+
+        //Simulo el giro de una moneda para asignar turno aleatoriamente al jugador que va a iniciar el juego
+        if(coin%2 == 0){
+			jugador = 'A';
+		}
+		else{
+			jugador = 'B';
+		}
+
+        printf("\nJuega %c", jugador);
+        printf("\n(M)over o (T)irar flecha?: ");
     }
+    while(vidaA == 0 || vidaB == 0);
 
     return 0;
 }
