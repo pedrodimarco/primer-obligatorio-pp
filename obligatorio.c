7833b3e68dd95d435dad73e8c530f70b2d09daea
pedrodimarco <68877732+pedrodimarco@users.noreply.github.com>
2021-05-31 22:16:24 -0300

    Implementacion de movimiento

--- obligatorio.c
+++ obligatorio.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 
+void clearBuffer() {
+    while(getchar() != '\n');
+}
+
 void generarBosque(char matriz[10][10]) {
 
     int pos1, pos2, cantArboles = 0;
@@ -49,7 +53,7 @@ void posicionarJugadores(char matriz[10][10]) {
 
 void reloadBosque(char matriz[10][10]) {
     int pos1, pos2;
-
+    printf("\n");
     for(pos2 = 0; pos2 <= 9; pos2++) {
 		
 	    for(pos1 = 0; pos1 <= 9; pos1++){ 
@@ -64,9 +68,11 @@ int main() {
     srand(time(NULL));
 
     int pos1, pos2, cantArboles = 0, vidaA = 1, vidaB = 1;
-    int coin = rand()%2; //Variable que simula el giro de una moneda con 2 resultados aleatorios posibles
-    char bosque[10][10];
-    char jugador;
+    int coin = rand()%2; //Variable que simula el giro de una coin con 2 resultados aleatorios posibles
+    int xA = 0, yA = 9, xB = 9, yB = 0, xJug, yJug; //Posiciones de jugadores A, B y generico
+    char comando; //Comando ingresado
+    char bosque[10][10]; //Tablero de juego
+    char jugador; //Jugador activo
 
     //Se colocan 10 arboles al azar
     //(Los arboles estan representados por los '0')
@@ -79,24 +85,110 @@ int main() {
 
     //Bucle del juego
     do { 
-        printf("\n");
-        
         //Imprimo el tablero de juego
-        
         reloadBosque(bosque);
 
-        //Simulo el giro de una moneda para asignar turno aleatoriamente al jugador que va a iniciar el juego
+        //Simulo el giro de una coin para asignar turno aleatoriamente al jugador que va a iniciar el juego
         if(coin%2 == 0){
 			jugador = 'A';
+            xJug = xA;
+            yJug = yA;
 		}
 		else{
 			jugador = 'B';
+            xJug = xB;
+            yJug = yB;
 		}
 
         printf("\nJuega %c", jugador);
-        printf("\n(M)over o (T)irar flecha?: ");
+        printf("\n(M)over o (T)irar flecha? (Tiro aun sin implementar): ");
+
+        comando = getchar();
+        clearBuffer();
+
+        //Verificar que el comando sea valido
+        if(comando != 'M' && comando != 'm' && comando != 'T' && comando != 't') {
+            printf("\n\nComando invalido, intente de nuevo");
+        }
+        else if(comando == 'M' || comando == 'm') {
+
+            //Consulta para donde desea mover el jugador
+            printf("\nDireccion: (N)orte, (S)ur, (E)ste, (O)este?: ");
+
+            comando = getchar();
+            clearBuffer();
+
+            //Verifica que el comando sea valido
+			if(comando != 'N' && comando != 'n' && comando != 'S' && comando != 's' && comando != 'E' && comando != 'e' && comando != 'O' && comando != 'o') {
+				printf("\n\nComando invalido, intente de nuevo");
+			}
+            else {
+                switch (comando) {
+                    case 'N':
+					case 'n':
+						if(yJug == 0 || bosque[xJug][yJug - 1] == '0' || bosque[xJug][yJug - 1] == 'A' || bosque[xJug][yJug - 1] == 'B'){
+							printf("\nMovimiento invalido, intente de nuevo");
+						}
+						else{
+							bosque[xJug][yJug - 1] = jugador;
+							bosque[xJug][yJug] = ' ';
+							yJug--;
+							coin++;
+						}
+						break;
+					case 'S':
+					case 's':
+						if(yJug == 9 || bosque[xJug][yJug + 1] == '0' || bosque[xJug][yJug + 1] == 'A' || bosque[xJug][yJug + 1] == 'B'){
+							printf("\nMovimiento invalido, intente de nuevo");
+						}
+						else{
+							bosque[xJug][yJug + 1] = jugador;
+							bosque[xJug][yJug] = ' ';
+							yJug++;
+							coin++;
+						}
+						break;
+					case 'E':
+					case 'e':
+						if(xJug == 9 || bosque[xJug + 1][yJug] == '0' || bosque[xJug + 1][yJug] == 'A' || bosque[xJug + 1][yJug] == 'B'){
+							printf("\nMovimiento invalido, intente de nuevo");
+						}
+						else{
+							bosque[xJug + 1][yJug] = jugador;
+							bosque[xJug][yJug] = ' ';
+							xJug++;
+							coin++;
+						}
+						break;
+					case 'O':
+					case 'o':
+						if(xJug == 0 || bosque[xJug - 1][yJug] == '0' || bosque[xJug - 1][yJug] == 'A' || bosque[xJug - 1][yJug] == 'B'){
+							printf("\nMovimiento invalido, intente de nuevo");
+						}	
+						else{
+							bosque[xJug - 1][yJug] = jugador;
+							bosque[xJug][yJug] = ' ';
+							xJug--;
+							coin++;
+						}
+						break;
+                }
+                //Actualizar posicion de jugadores
+                switch(jugador) {
+					case 'A':
+						xA = xJug;
+						yA = yJug;
+					break;
+					case 'B':
+						xB = xJug;
+						yB = yJug;
+					break;
+				}
+            }//(Fin else que ejecuta el movimiento)
+        }
+        
     }
-    while(vidaA == 0 || vidaB == 0);
+    while(vidaA == 1 && vidaB == 1);
 
     return 0;
 }
